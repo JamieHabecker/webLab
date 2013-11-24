77574e212eb22234f6329e21101223e7319c42e8
chapman21 <trevorlchapman@gmail.com>
2013-11-24 11:32:45 -0500

    Added odometerFraus app

---
+++ devApps/odometerFraud/scripts/vendor/ga.js
@@ -0,0 +1,27 @@
+/*
+    var _gaq = _gaq || [];
+    var testKey = 'qeTest', storage = window.sessionStorage;
+  _gaq.push(['_setAccount', 'UA-38638778-1']);
+  _gaq.push(['_trackPageview']);
+    (function() {
+    var s, ga = document.createElement('script'); 
+    ga.type = 'text/javascript'; 
+    ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+    
+    try {
+        // Try and catch quota exceeded errors
+        storage.setItem(testKey, '1');
+        storage.removeItem(testKey);
+    } catch (error) {
+        if (error.code === DOMException.QUOTA_EXCEEDED_ERR && storage.length === 0)
+            alert('Please make sure you have private browsing turned off in your Safari settings to use this form.');
+        else
+            throw error;
+    }
+	
+
+
+*/
