06eb263153ca0db6ec016dfb795308603fe35aea
chapman21 <trevorlchapman@gmail.com>
2013-11-29 11:27:38 -0500

    Cleaned spaces

--- public/globals/factories.js
+++ public/globals/factories.js
@@ -4,12 +4,12 @@ angular.module("globalFactories", [])
 
 
 .factory('StateFactory',['$resource', function($resource) {
-    var baseUrl = dev + "/globals/models/states.json";
-    return $resource(baseUrl, {}, {
-        menu: {
-            method : 'GET',
-            isArray : true,
-            url : baseUrl
-        }
-    });
+		var baseUrl = dev + "/globals/models/states.json";
+			return $resource(baseUrl, {}, {
+				menu: {
+					method : 'GET',
+					isArray : true,
+					url : baseUrl
+				}
+			});
 }])
