868a668fe35ef335bc8e96652b3dcb353080e135
chapman21 <trevorlchapman@gmail.com>
2013-11-29 11:11:41 -0500

    Clean up spaces

--- public/globals/directives.js
+++ public/globals/directives.js
@@ -3,19 +3,19 @@ angular.module("globalDirectives", [])
 
 
 .directive('states', function(StateFactory){
-    return{
-        restrict: "A",
-        template: "<label>State<span class='reqText'>Required</label><select data-ng-model='state' data-ng-click='setState()' data-ng-options='c.State for c in states'>" +
-        "<option value=''>{{current}}</option></select>",
-         link: function(scope, elm, attrs){
-             StateFactory.menu({},{}, statescb, stateserr);
-    						function statescb(data){
-      						scope.states = data;
-    						}
-    						function stateserr(err){
-      						console.log(err)
-    						}
-         }
-    };
-})
+			return{
+				restrict: "A",
+				template: "<label>State<span class='reqText'>Required</label><select data-ng-model='state' data-ng-click='setState()' data-ng-options='c.State for c in states'>" +
+						"<option value=''>{{current}}</option></select>",
+				link: function(scope, elm, attrs){
+					StateFactory.menu({},{}, statescb, stateserr);
+					function statescb(data){
+						scope.states = data;
+					}
+					function stateserr(err){
+						console.log(err)
+					}
+				}
+			};
+		})
 
