6fc2c98ca18d4cd667e02478f8069c09a83b1cf5
chapman21 <trevorlchapman@gmail.com>
2013-12-01 08:31:38 -0500

    Trying to globalize state directive

--- public/globals/factories.js
+++ public/globals/factories.js
@@ -1,7 +1,7 @@
 angular.module("globalFactories", [])
 .factory('StateFactory',['$resource', function($resource) {
-		var dev = "http://localhost";
-		var baseUrl = dev + "/globals/models/states.json";
+		var dev = window.location.protocol + "//" + window.location.host + "/";
+		var baseUrl = dev + "globals/models/states.json";
 			return $resource(baseUrl, {}, {
 				menu: {
 					method : 'GET',
