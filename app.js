d47b706998dd617e55cc1865d9c89a1e61686a53
chapman21 <trevorlchapman@gmail.com>
2013-12-06 11:25:42 -0500

    Fixing weblab

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 var express = require('express');
 var routes = require('./routes');
-var routes = require('jade');
+//
 var mydmv = require('./routes/myDMV');
 var http = require('http');
 var path = require('path');
