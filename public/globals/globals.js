2ec1e7e180842767a8281944987f637e8cfdeccf
chapman21 <trevorlchapman@gmail.com>
2013-12-16 10:18:28 -0500

    Adjustment for weblab grunt

--- public/globals/globals.js
+++ public/globals/globals.js
@@ -27658,7 +27658,7 @@ angular.module('ngResource', ['ng']).
 
 .factory('StateFactory',['$resource', function($resource) {
 		var dev = window.location.protocol + "//" + window.location.host + "/";
-		var baseUrl = dev + "globals/models/states.json";
+		var baseUrl = dev + "models/states.json";
 			return $resource(baseUrl, {}, {
 				menu: {
 					method : 'GET',
--- public/globals/scripts/factories.js
+++ public/globals/scripts/factories.js
@@ -10,7 +10,7 @@ angular.module("globalFactories", [])
 
 .factory('StateFactory',['$resource', function($resource) {
 		var dev = window.location.protocol + "//" + window.location.host + "/";
-		var baseUrl = dev + "globals/models/states.json";
+		var baseUrl = dev + "models/states.json";
 			return $resource(baseUrl, {}, {
 				menu: {
 					method : 'GET',
