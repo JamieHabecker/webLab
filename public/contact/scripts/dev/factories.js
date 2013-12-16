aea2ff1641d853dce8e6570d4b566ab858a069fb
chapman21 <trevorlchapman@gmail.com>
2013-12-16 16:09:04 -0500

    Adding new connection for contact us

--- public/contact/scripts/dev/factories.js
+++ public/contact/scripts/dev/factories.js
@@ -9,7 +9,7 @@ angular.module("factories", [])
 
 
 .factory('ContactFactory',['$resource', function($resource) {
-    var baseUrl = "/apps/ContactUs/Default.aspx/SendFields";
+    var baseUrl = "http://10.156.147.121:444\:443/WebServicesBackEnd/ContactUs.aspx/SendFields";
     return $resource(baseUrl, {}, {
         contactInfo : {
             method : 'Post',
