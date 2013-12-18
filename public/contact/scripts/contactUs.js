6fc5636dfd22cff1d1165495c2f59b7a5273a16d
chapman21 <trevorlchapman@gmail.com>
2013-12-18 09:35:21 -0500

    Latest builds for contact and odometerfraud

--- public/contact/scripts/contactUs.js
+++ public/contact/scripts/contactUs.js
@@ -442,16 +442,14 @@ angular.module("directives", [])
 }]); ;angular.module("factories", [])
 
 
-
-
 .factory('message', function() {
     return []
 })
 
 
 .factory('ContactFactory',['$resource', function($resource) {
-    var baseUrl = "http://10.156.147.121:443\:443/WebServicesBackEnd/ContactUs.aspx/SendFields";
-		//var baseUrl = "/apps/WebServicesBackEnd/ContactUs.aspx/SendFields";
+    //var baseUrl = "http://10.156.147.121:443\:443/WebServicesBackEnd/ContactUs.aspx/SendFields";
+		var baseUrl = "/apps/WebServicesBackEnd/ContactUs.aspx/SendFields";
     return $resource(baseUrl, {}, {
         contactInfo : {
             method : 'Post',
