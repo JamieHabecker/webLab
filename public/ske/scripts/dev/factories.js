6e115e286cfaeb0ca4281841d4d165a0fe252d87
chapman21 <trevorlchapman@gmail.com>
2013-12-19 13:17:32 -0500

    SKE method changed

--- public/ske/scripts/dev/factories.js
+++ public/ske/scripts/dev/factories.js
@@ -25,7 +25,7 @@ angular.module("factories", [])
 })
 
 		.factory('knowledgeFactory',['$resource', function($resource) {
-		var baseUrl = "http://10.156.147.121:443\:443/WebServicesBackEnd/SampleKnowledgeExam.aspx/Getquestions";
+		var baseUrl = "http://10.156.147.121:443\:443/WebServicesBackEnd/SampleKnowledgeExam.aspx/GetSKE";
 		//var baseUrl = "/apps/WebServicesBackEnd/SampleKnowledgeExam.aspx/Getquestions";
 			return $resource(baseUrl, {}, {
 				skeQuestions : {
