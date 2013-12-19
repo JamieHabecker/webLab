ce1a9342795205aa5aa0fea51fb4e0085e96270b
chapman21 <trevorlchapman@gmail.com>
2013-12-19 11:09:55 -0500

    Redo conn for contact us dev

--- public/contact/scripts/dev/factories.js
+++ public/contact/scripts/dev/factories.js
@@ -7,8 +7,8 @@ angular.module("factories", [])
 
 
 .factory('ContactFactory',['$resource', function($resource) {
-    //var baseUrl = "http://10.156.147.121:443\:443/WebServicesBackEnd/ContactUs.aspx/SendFields";
-		var baseUrl = "/apps/WebServicesBackEnd/ContactUs.aspx/SendFields";
+    var baseUrl = "http://10.156.147.121:443\:443/WebServicesBackEnd/ContactUs.aspx/SendFields";
+		//var baseUrl = "/apps/WebServicesBackEnd/ContactUs.aspx/SendFields";
     return $resource(baseUrl, {}, {
         contactInfo : {
             method : 'Post',
