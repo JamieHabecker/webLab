3b2fa3c26c108bd0c288ee61994a4f8c29f9d0fc
chapman21 <trevorlchapman@gmail.com>
2013-12-30 10:26:19 -0500

    reset dev conn

--- public/SalvageDealerComplaint/scripts/dev/factories.js
+++ public/SalvageDealerComplaint/scripts/dev/factories.js
@@ -5,8 +5,8 @@ angular.module("factories", [])
 })
 
 .factory('ContactFactory',['$resource', function($resource) {
-		//var baseUrl = "http://10.156.147.121:443\:443/WebServicesBackEnd/SalvageComplaint.aspx/SendFields";
-		var baseUrl = "/apps/WebServicesBackEnd/SalvageComplaint.aspx/SendFields";
+		var baseUrl = "http://10.156.147.121:443\:443/WebServicesBackEnd/SalvageComplaint.aspx/SendFields";
+		//var baseUrl = "/apps/WebServicesBackEnd/SalvageComplaint.aspx/SendFields";
     return $resource(baseUrl, {}, {
         contactInfo : {
             method : 'Post',
