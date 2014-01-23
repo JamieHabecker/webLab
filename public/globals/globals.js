3c67d2cced00be1b93cc289c39454235e07bfd9e
chapman21 <trevorlchapman@gmail.com>
2014-01-23 10:43:59 -0500

    Adding link to dmvnow app

--- public/globals/globals.js
+++ public/globals/globals.js
@@ -34031,23 +34031,6 @@ angular.module('ngResource', ['ng']).
 			});
 }])
 
-/*
-.animation('.an-enter', function() {
-			return {
-				setup : function(myElement) {
-					myElement.css({ 'opacity': 0.3 });
-					return {}; //if you want to share some dat between the set and start return it it can be anything
-				},
-				start : function(myElement, done, data) {
-					myElement.animate({
-						'opacity' : 1
-					}, 300, function(){
-						done();
-					});
-				}
-			}
-})
-*/
 
 .animation('.siteAnime', function() {
 	return {
